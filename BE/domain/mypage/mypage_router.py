48c34c2bb1217e98f007da5889a6ebeffb90d23f
noFlowWater <noyusu@knu.ac.kr>
2023-12-01 05:08:49 +0900

    fix: MyPage Router코드 수정.

--- BE/domain/mypage/mypage_router.py
+++ BE/domain/mypage/mypage_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi import APIRouter, Depends, HTTPException
 import domain.mypage.mypage_crud as mypage_crud
 from db import get_db_connection
 from util import get_current_user_id
@@ -11,7 +11,7 @@ router = APIRouter(
 def read_current_user_profile(conn=Depends(get_db_connection), user_id: str = Depends(get_current_user_id)):
     user_json = mypage_crud.get_user(user_id, conn)
     if user_json == "{}":
-        raise Response(status_code=404, detail="No user found")
+        raise HTTPException(status_code=404, detail="No user found")
     return user_json
 
 @router.get("/rooms")
@@ -25,12 +25,12 @@ def read_user_rooms(conn=Depends(get_db_connection), user_id: str = Depends(get_
 def read_user_posts(conn=Depends(get_db_connection), user_id: str = Depends(get_current_user_id)):
     posts_json = mypage_crud.get_user_posts(user_id, conn)
     if posts_json == "[]":
-        raise Response(status_code=204, detail="No posts found")
+        raise HTTPException(status_code=204, detail="No posts found")
     return posts_json
 
 @router.get("/wishes")
 def read_user_wishes(conn=Depends(get_db_connection), user_id: str = Depends(get_current_user_id)):
     wishes_json = mypage_crud.get_user_wishes(user_id, conn)
     if wishes_json == "[]":
-        raise Response(status_code=204, detail="No wishes found")
+        raise HTTPException(status_code=204, detail="No wishes found")
     return wishes_json
